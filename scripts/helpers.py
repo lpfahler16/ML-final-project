fdfe8f98e5cae89debae6c5160413abdf7d64810
LPfahler Macbook Pro <46872232+lpfahler16@users.noreply.github.com>
2023-04-12 20:02:11 -0400

    Refactor

---
+++ scripts/helpers.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+df_2018 = pd.read_csv('../data/2018.csv')
+df_2017 = pd.read_csv('../data/2017.csv')
+df_2016 = pd.read_csv('../data/2016.csv')
+
+
+def get_classifier_convert(df):
+    conditions = [
+        (df['fourth_down_converted'] == 1.0),
+        (df['fourth_down_failed'] == 1.0),
+        (df['field_goal_attempt'] == 1.0),
+        (df['punt_attempt'] == 1.0)
+    ]
+
+    results = ['CONVERTED', 'FAILED', 'FIELD_GOAL', 'PUNT']
+
+    y = np.select(conditions, results)
+    return y
+
+
+def get_classifier_attempt(df):
+    conditions = [
+        (df['fourth_down_converted'] == 1.0) | (
+            df['fourth_down_failed'] == 1.0),
+        (df['field_goal_attempt'] == 1.0),
+        (df['punt_attempt'] == 1.0)
+    ]
+
+    results = ['ATTEMPTED', 'FIELD_GOAL', 'PUNT']
+
+    y = np.select(conditions, results)
+    return y
+
+
+def ready_data_convert(df):
+    df = df.dropna()
+    y = get_classifier_convert(df)
+    df = df.drop(columns=['posteam', 'fourth_down_converted', 'fourth_down_failed',
+                 'field_goal_attempt', 'punt_attempt', 'game_date', 'down'])
+    return df, y
+
+
+def ready_data_attempt(df):
+    df = df.dropna()
+    y = get_classifier_attempt(df)
+    df = df.drop(columns=['posteam', 'fourth_down_converted', 'fourth_down_failed',
+                 'field_goal_attempt', 'punt_attempt', 'game_date', 'down'])
+    return df, y
+
+
+def attempt_data_split():
+    return data_split(ready_data_attempt)
+
+
+def convert_data_split():
+    return data_split(ready_data_convert)
+
+
+def data_split(func):
+    x, y = func(pd.concat([df_2016, df_2017, df_2018]))
+    X_train, X_test, y_train, y_test = train_test_split(
+        x, y, test_size=0.2, random_state=10)
+    return X_train, X_test, y_train, y_test
